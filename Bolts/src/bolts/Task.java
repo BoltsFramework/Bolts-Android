e5e428e5174260b0a2dc0ae2a7b69454ed6c219d
Grantland Chew <grantlandchew@gmail.com>
2014-08-19 10:09:49 -0700

    Hide AndroidExecutors and BoltsExecutors

--- Bolts/src/bolts/Task.java
+++ Bolts/src/bolts/Task.java
@@ -18,8 +18,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import bolts.android.AndroidExecutors;
-
 /**
  * Represents the result of an asynchronous operation.
  * 
