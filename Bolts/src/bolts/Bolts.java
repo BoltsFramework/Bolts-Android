ed68b0a6476ec70985235eaa57cd277135a718e1
Grantland Chew <grantlandchew@gmail.com>
2015-07-15 16:11:43 -0700

    Prepare for next development iteration

--- Bolts/src/bolts/Bolts.java
+++ Bolts/src/bolts/Bolts.java
@@ -16,5 +16,5 @@ public final class Bolts {
   /**
    * The version of the Bolts library.
    */
-  public static final String VERSION = "1.2.1";
+  public static final String VERSION = "1.2.2-SNAPSHOT";
 }
