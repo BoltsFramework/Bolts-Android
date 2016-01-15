3def0de57eb06ea09056251be93e397ea28e34c1
Grantland Chew <grantlandchew@gmail.com>
2016-01-15 14:33:49 -0800

    :zap: Release 1.4.0 :zap:

--- bolts-applinks/src/main/java/bolts/Bolts.java
+++ bolts-applinks/src/main/java/bolts/Bolts.java
@@ -16,5 +16,5 @@ public final class Bolts {
   /**
    * The version of the Bolts library.
    */
-  public static final String VERSION = "1.2.2-SNAPSHOT";
+  public static final String VERSION = "1.4.0";
 }
