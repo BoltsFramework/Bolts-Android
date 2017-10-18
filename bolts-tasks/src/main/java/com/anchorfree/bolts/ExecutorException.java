c3b614a759ee499e12cbdb43740897bc94da1f46
Alexandr Timoshenko <thick.tav@gmail.com>
2017-10-18 13:20:40 +0200

    changed package name to com.anchorfree.bolts

---
+++ bolts-tasks/src/main/java/com/anchorfree/bolts/ExecutorException.java
@@ -0,0 +1,12 @@
+package com.anchorfree.bolts;
+
+/**
+ * This is a wrapper class for emphasizing that task failed due to bad {@code Executor}, rather than
+ * the continuation block it self.
+ */
+public class ExecutorException extends RuntimeException {
+
+  public ExecutorException(Exception e) {
+    super("An exception was thrown by an Executor", e);
+  }
+}
