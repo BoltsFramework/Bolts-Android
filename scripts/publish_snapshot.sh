ab44482e7ed42287fd9a527365f05d28a52ec9c8
Daniel H <daniel.hugenroth@gmail.com>
2019-02-20 11:57:16 +0000

    Add missing (c) headers (#155)

--- scripts/publish_snapshot.sh
+++ scripts/publish_snapshot.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the/
+# LICENSE file in the root directory of this source tree.
+
 # Publishes SNAPSHOTs
 
 REPO_SLUG=BoltsFramework/Bolts-Android
@@ -14,7 +20,6 @@ elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping publishing SNAPSHOT: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'"
 else
   echo "Publishing SNAPSHOT..."
-  ./gradlew uploadArchives  
+  ./gradlew uploadArchives
   echo "SNAPSHOT published!"
 fi
-
