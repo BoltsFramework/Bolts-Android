54e9cb8bdd4950aa4d418dcbc0ea65414762aef5
Daniel Hugenroth <hugenroth@fb.com>
2019-02-22 11:41:39 +0000

    Update version to 1.5.0

--- bolts-applinks/src/main/java/bolts/Bolts.java
+++ bolts-applinks/src/main/java/bolts/Bolts.java
@@ -13,5 +13,5 @@ public final class Bolts {
   /**
    * The version of the Bolts library.
    */
-  public static final String VERSION = "1.4.1-SNAPSHOT";
+  public static final String VERSION = "1.5.0";
 }
